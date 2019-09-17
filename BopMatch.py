c17176496d07082743e3240899bd086f3f138a9c
Blair <1234blair@gmail.com>
2019-09-17 12:49:40 -0700

    Made code readable. Split repeated sections into functions. Added comments.
    Usernames and match type are now input as cmd line arguments.

--- BopMatch.py
+++ BopMatch.py
@@ -1,67 +1,98 @@
 import spotipy
 import spotipy.util as util
+import argparse
 
-checkArtist = 1
-checkAlbum = 0
-checkID = 0
 
-username = 'fissionist'
-username2 = 'madeline701'
-playlistName = 'Bop Match: ' + username + ' x ' + username2
-token = util.prompt_for_user_token(username, scope='playlist-modify-public', client_id='409994c70c5d4e719eb2c4104755edb5', client_secret='1260f9d85a9f44bc9747f788d4c00a16', redirect_uri='http://localhost/callback')
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-
-    user1Tracks = []
+def get_playlist_tracks(username):
+    '''Download user playlist data, extract all tracks, and return as a list'''
+    trackList = []
 
     playlists = sp.user_playlists(username)
     for playlist in playlists['items']:
         if playlist['owner']['id'] == username:
+            print(playlist['name'])
+            playlistData = sp.user_playlist(username, playlist['id'])
+            for track in enumerate(playlistData['tracks']['items']):
+                trackList.append(track[1]['track'])
 
-            print (playlist['name'])
-            playlistTracks = sp.user_playlist(username, playlist['id'])
-            for track in enumerate(playlistTracks['tracks']['items']):
-                user1Tracks.append(track[1]['track'])
+    return trackList
 
+
+def match_tracks(trackList1, trackList2, matchType):
+    '''Find tracks from track lists that match based on track ID (0), album (1), or artist (2)
+    Return IDs of matching tracks.
+    '''
     matchingTrackIDs = []
 
-    playlists = sp.user_playlists(username2)
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == username2:
+    for track1 in enumerate(trackList1):
+        for track2 in enumerate(trackList2):
 
-            print (playlist['name'])
-            playlistTracks = sp.user_playlist(username, playlist['id'])
-            for track in enumerate(playlistTracks['tracks']['items']):
+            if matchType == 2:
+                for artist1 in enumerate(track1[1]['artists']):
+                    for artist2 in enumerate(track2[1]['artists']):
+                        if artist1[1]['id'] == artist2[1]['id']:
+                            matchingTrackIDs.append(track1[1]['id'])
 
-                if (checkArtist == 1):
-                    for i in enumerate(track[1]['track']['artists']):
-                        for j in enumerate(user1Tracks):
-                            for k in enumerate(j[1]['artists']):
-                                if(i[1]['id'] == k[1]['id']):
-                                    matchingTrackIDs.append(track[1]['track']['id'])
+            elif matchType == 1:
+                if track1[1]['album']['id'] == track2[1]['album']['id']:
+                    matchingTrackIDs.append(track1[1]['id'])
 
-                elif (checkAlbum == 1 and any(x['album']['name'] == track[1]['track']['album']['name'] for x in user1Tracks)):
-                    matchingTrackIDs.append(track[1]['track']['id'])
+            elif matchType == 0:
+                if track1[1]['id'] == track2[1]['id']:
+                    matchingTrackIDs.append(track1[1]['id'])
 
-                elif (checkID == 1 and any(x['id'] == track[1]['track']['id'] for x in user1Tracks)):
-                    matchingTrackIDs.append(track[1]['track']['id'])
-                
+    return matchingTrackIDs
+
+
+def chunk_track_list(trackList):
+    '''Split list of tracks into 100 track chunks.  
+    Spotify only allows 100 tracks to be added to a playlist at a time.
+    '''
+    for i in range(0, len(trackList), 100):
+        yield trackList[i:i + 100]
+
+
+parser = argparse.ArgumentParser(description='Find matching tracks between two Spotify user accounts.')
+parser.add_argument('username1', type=str, help='Username of first account for track matching. The BopMatch playlist is created in username1\'s library. Authentication for the account will be required.')
+parser.add_argument('username2', type=str, help='Username of second account for track matching. Only used for track matching, no authentication required.')
+parser.add_argument('matchType', type=int, help='0 = Match by Track, 1 = Match by Album, 2 = Match by Artist')
+args = parser.parse_args()
+
+#Obtain permission to create and modify playlists for username1
+token = util.prompt_for_user_token(args.username1, scope='playlist-modify-public', client_id='409994c70c5d4e719eb2c4104755edb5', client_secret='1260f9d85a9f44bc9747f788d4c00a16', redirect_uri='http://localhost/callback')
+if token:
+    sp = spotipy.Spotify(auth=token)
+
+    #Download user playlist data and extract all tracks
+    user1Tracks = get_playlist_tracks(args.username1)
+    user2Tracks = get_playlist_tracks(args.username2)
+
+    #Find matching tracks by artist, album, or track
+    matchingTrackIDs = []
+    matchingTrackIDs.extend(match_tracks(user1Tracks, user2Tracks, args.matchType))
+    matchingTrackIDs.extend(match_tracks(user2Tracks, user1Tracks, args.matchType))
+    
+    #Remove any duplicate track IDs
     matchingTrackIDs = list(set(matchingTrackIDs))
-    print(matchingTrackIDs)
 
     if (len(matchingTrackIDs) > 0):
-        sp.user_playlist_create(username, playlistName)
+        #Create BopMatch playlist
+        playlistName = 'Bop Match: ' + args.username1 + ' x ' + args.username2
+        sp.user_playlist_create(args.username1, playlistName)
 
-        playlists = sp.user_playlists(username)
+        #Find BopMatch playlist ID
+        playlists = sp.user_playlists(args.username1)
         for playlist in playlists['items']:
-            if (playlist['owner']['id'] == username and playlist['name'] == playlistName):
+            if (playlist['owner']['id'] == args.username1 and playlist['name'] == playlistName):
                 playlistID = playlist['id']
 
-        sp.user_playlist_add_tracks(username, playlistID, matchingTrackIDs)
+        #Split track list into 100 track chunks and add to playlist
+        matchingTrackIDs = chunk_track_list(matchingTrackIDs)    
+        for trackChunk in enumerate(matchingTrackIDs):
+            sp.user_playlist_add_tracks(args.username1, playlistID, trackChunk[1])
+
     else:
         print("No matching songs found :(")
 
 else:
-    print ("Can't get token for", username)
-
+    print ("Can't get token for", args.username1)
